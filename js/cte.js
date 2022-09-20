d568bcc1372d03665eb7699d124efe45fedce71a
michael-kisida <michael@kisida.com>
2022-09-19 22:46:36 -0400

    VIS Site

--- js/cte.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-  console.log("cte.js is active.");
-  
-  $("#learnMore").click(function(){
-    $("html,body").velocity({
-      scrollTop: $("#cteAbout").offset().top
-    }, 600);
-  });
-});
